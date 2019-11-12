410bcc3fd59d4a0a66515e5fb86280271ce7112e
Tadieg <47139816+Tadieg@users.noreply.github.com>
2019-11-12 13:33:34 +0300

    Update fibonanciBypointer.c

--- Tadie/fibonanciBypointer.c
+++ Tadie/fibonanciBypointer.c
@@ -7,21 +7,9 @@ int main(){
 	printf("enter the number n  ");
 	scanf("%d",&n);
 	printf("===============fibonaci by value:==================\n");
-  fibonaciByValue(n);
-  fibonaciByReference(&n);
+        fibonaciByReference(&n);
 }
- void fibonaciByValue(int n){
- 	int i;
- 	int arr[n];
- 	arr[0]=0,arr[1]=1;
-	for(i=2;i<n;i++){
-	arr[i]=arr[i-1]+arr[i-2];
-	}
-	for(i=0;i<n;i++){
-		printf("%d\n",arr[i]);
-	}
 
-}
  void fibonaciByReference(int*n){
  	int i;
  	int arr[*n];
